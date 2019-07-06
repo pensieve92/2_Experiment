ee10f2b26ba7037f3469212648367786446192ab
JeonBH <pensieve92@gmail.com>
2019-07-06 14:01:39 +0900

    dbconnection test code summary
    execute exception

--- 2_Experiment/src/test/java/db/DBConnection.java
+++ 2_Experiment/src/test/java/db/DBConnection.java
@@ -1,5 +1,7 @@
 package db;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -12,28 +14,27 @@ public class DBConnection {
 	@Test
 	public void DBConnectionTest() {
 
-		//ojdbc 추가하기 
-		//C:\Program Files\ojdkbuild\java-1.8.0-openjdk-1.8.0.212-1\jre\lib\ext
-		//ojdbc6.jar
-
-
-
-	/*	DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver()); 
-		Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@211.56.251.176:1521:ORA9","laputa","laputa"); 
-		DatabaseMetaData meta = conn.getMetaData (); */
-
-		
-		
-		
-		Connection conn = null;
-        try {
+		//ojdbc6.jar 추가하기 
+		//C:\Program Files\ojdkbuild\java-1.8.0-openjdk-1.8.0.212-1\jre\lib\ext\ojdbc6.jar
+			
+        try {        	
+        	
+        	//given
             String user = "JBH"; 
             String pw = "1234";
             String url = "jdbc:oracle:thin:@123.213.29.5:1521:orcl";
             
-            Class.forName("oracle.jdbc.driver.OracleDriver");        
-            conn = DriverManager.getConnection(url, user, pw);
+            Class.forName("oracle.jdbc.driver.OracleDriver");
             
+			//work           
+            Connection conn = DriverManager.getConnection(url, user, pw);
+            
+            //Exception
+            /*throw new ClassNotFoundException();*/
+            /*throw new SQLException();*/
+            
+			//result
+            assertNotNull(conn);            
             System.out.println("Database에 연결되었습니다.\n");
             
         } catch (ClassNotFoundException cnfe) {
@@ -44,13 +45,5 @@ public class DBConnection {
             System.out.println("Unkonwn error");
             e.printStackTrace();
         }
-          
-
-		
-	
 	}
-	
-	
-	
-
 }
